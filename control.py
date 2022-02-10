ccf5db839443395b6ae8c4eda22c13e65eb75cbe
fabiod <sZ143Kfq8T>
2022-02-10 15:25:06 +0000

    Changed approach to launch fall3d and hysplit jobs

--- control.py
+++ control.py
@@ -1011,25 +1011,16 @@ def run_models(short_simulation, eruption_dur):
                 command_setsrc = FALL3D + ' SetSrc ' + INPUT
                 command_fall3d = 'mpirun -n ' + str(np) + ' ' + FALL3D + ' Fall3D ' + INPUT + ' ' + str(npx) + ' ' + \
                                  str(npy) + ' ' + str(npz)
-                # if which('sbatch') is None:
-                #     os.system(command_setdbs)
-                #     os.system(command_setsrc)
-                #     os.system(command_fall3d)
-                # else:
-                #     os.system('srun -J FALL3D_SetDbs -n ' + str(np) + ' ' + command_setdbs)
-                #     os.system('srun -J FALL3D_SetSrc -n 1 ' + command_setsrc)
-                #     os.system('srun -J FALL3D -n ' + str(np) + ' ' + command_fall3d)
-                if which('salloc') != None:
-                    os.system('salloc -n ' + str(np) + ' -J FALL3D &> log_slurm.txt &')
-                    with open('log_slurm.txt', 'r') as slurm_log:
-                        for line in slurm_log:
-                            if 'Granted job allocation' in line:
-                                job_id = line.split('Granted job allocation ')[1]
-                os.system(command_setdbs)
-                os.system(command_setsrc)
-                os.system(command_fall3d)
-                if which('sbatch') != None:
-                    os.system('scancel ' + job_id)
+                os.chdir(RUN)
+                if which('srun') is None:
+                    os.system(command_setdbs)
+                    os.system(command_setsrc)
+                    os.system(command_fall3d)
+                else:
+                    os.system('srun -J FALL3D_SetDbs -n ' + str(np) + ' ' + command_setdbs)
+                    os.system('srun -J FALL3D_SetSrc -n 1 ' + command_setsrc)
+                    os.system('srun -J FALL3D -n ' + str(np) + ' ' + command_fall3d)
+                os.chdir(ROOT)
 
             try:
                 pool_fall3d = ThreadingPool(len(solutions))
@@ -1315,25 +1306,15 @@ def run_models(short_simulation, eruption_dur):
                 np = n_processes / len(solutions)
                 if np > n_bins:
                     np = n_bins
-                if which('salloc') != None:
-                    os.system('salloc -n ' + str(np) + ' -J HYSPLIT &> log_slurm.txt &')
-                    with open('log_slurm.txt', 'r') as slurm_log:
-                        for line in slurm_log:
-                            if 'Granted job allocation' in line:
-                                job_id = line.split('Granted job allocation ')[1]
-                os.system('mpirun -np ' + '{:.0f}'.format(np) + ' ' + os.path.join(HYSPLIT, 'hycm_std'))
-                os.system(os.path.join(HYSPLIT, 'con2cdf4') + ' ' + os.path.join(OUT, 'cdump') + ' ' +
-                          os.path.join(OUT, 'cdump.nc'))
-                if which('sbatch') != None:
-                    os.system('scancel ' + job_id)
-
-                # if which('srun') is None:
-                #     os.system('mpirun' + ' -np ' +'{:.0f}'.format(np) + ' ' + os.path.join(HYSPLIT, 'hycm_std'))
-                # else:
-                #     os.system('srun -J HYSPLIT -n ' + '{:.0f}'.format(np) + ' mpirun' + ' -np ' +'{:.0f}'.format(np)
-                #               + ' ' + os.path.join(HYSPLIT, 'hycm_std'))
-                # os.system('srun -J HYSPLIT_con2cdf4 ' + os.path.join(HYSPLIT, 'con2cdf4') + ' ' +
-                #           os.path.join(OUT, 'cdump') + ' ' + os.path.join(OUT, 'cdump.nc'))
+                command_hycm = 'mpirun -np ' + '{:.0f}'.format(np) + ' ' + os.path.join(HYSPLIT, 'hycm_std')
+                command_con2cdf4 = os.path.join(HYSPLIT, 'con2cdf4') + ' ' + os.path.join(OUT, 'cdump') + ' ' + \
+                                   os.path.join(OUT, 'cdump.nc')
+                if which('run') is None:
+                    os.system(command_hycm)
+                    os.system(command_con2cdf4)
+                else:
+                    os.system('srun -J HYSPLIT -n ' + '{:.0f}'.format(np) + ' ' + command_hycm)
+                    os.system('srun -J HYSPLIT -n 1 '+ command_con2cdf4)
                 os.chdir(ROOT)
 
 
