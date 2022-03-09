672085bdd46313d3dd4e577c7592dd307952aafb
FabioDioguardi <fabiod@bgs.ac.uk>
2022-03-09 16:48:20 +0000

    Added a command to restore the original weather script after each simulation performed with run_models.sh

--- run_models.sh
+++ run_models.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 shr=$(date -u +%H)
-#if [ $shr -eq 0 ] || [ $shr -eq 6 ] || [ $shr -eq 12 ] || [ $shr -eq 18 ]; then
-#    echo $shr
-#else
-#    exit
-#fi
+if [ $shr -eq 0 ] || [ $shr -eq 6 ] || [ $shr -eq 12 ] || [ $shr -eq 18 ]; then
+    echo $shr
+else
+    exit
+fi
 
 source ~/.bashrc
 conda activate ash_dispersion_modelling
@@ -13,4 +13,5 @@ python weather.py > log/log_weather_$shr.txt # Download weather data
 python control.py --set=True > log/log_models_$shr.txt # Add options
 conda deactivate
 python post_processing.py --set=True --mode=operational > log/log_post_processing_$shr.txt
+cp /home/vulcanomod/BGS-AADM/weather/scripts/*.sh weather/scripts/
 
